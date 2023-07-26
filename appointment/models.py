a0018708ba0b1f457ed420f2d46a8cd1c8eac215
Zarin-16 <tasnimzarin427@gmail.com>
2023-07-26 21:44:28 +0600

    phone number added

--- appointment/models.py
+++ appointment/models.py
@@ -9,6 +9,7 @@ class Appointment(models.Model):
     patient_age = models.CharField(max_length=3)
     patient_email=models.EmailField(max_length=50)
     patient_gender= models.CharField(max_length=7)
+    phone_number = models.CharField(max_length=15)
     department_name = models.ForeignKey(Departments, on_delete=models.CASCADE,related_name="department_names",verbose_name="Department Name")
     doctor_name = models.ForeignKey(Doctor, on_delete=models.CASCADE,related_name="doctor_name",verbose_name="Doctor Name")
     appoinment_date = models.DateField(verbose_name='Appoinment Date', null=True)
